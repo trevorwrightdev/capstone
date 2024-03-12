ffd142b92afd9d2d72b7756a03459b1a9a2639a7
trevdev <trevorwrightdev@gmail.com>
2024-03-12 15:13:06 -0700

    save

--- capstone-frontend/app/data/page.jsx
+++ capstone-frontend/app/data/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import barGraph from '../../public/bar-graph.png'
 import wordCloud from '../../public/word-cloud.png'
+import pieChart from '../../public/pie-chart.png'
 
 export default function Data() {
     return (
@@ -12,7 +13,7 @@ export default function Data() {
                     width={1024}
                     height={768}
                 />
-                <p className='text-white text-center'>Bar Graph</p>
+                <p className='text-white text-center'>Bar graph with notable words from the dataset</p>
             </div>
             <div className='flex flex-col items-center gap-2 w-[90%] md:w-[600px]'>
                 <Image
@@ -21,7 +22,16 @@ export default function Data() {
                     width={2560}
                     height={1440}
                 />
-                <p className='text-white text-center'>Word Cloud</p>
+                <p className='text-white text-center'>Word cloud with notable words from the dataset</p>
+            </div>
+            <div className='flex flex-col items-center gap-2 w-[90%] md:w-[600px]'>
+                <Image
+                    className='w-full rounded-md'
+                    src={pieChart}
+                    width={2560}
+                    height={1440}
+                />
+                <p className='text-white text-center'>Pie graph showing the dominance of the top 30% of words in the dataset</p>
             </div>
         </div>
     )
