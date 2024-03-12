14da38b936fe53449b00b0f164a09baf1cb6ba02
trevdev <trevorwrightdev@gmail.com>
2024-03-12 15:27:38 -0700

    fixed mobile navbar

--- capstone-frontend/app/components/Navbar.jsx
+++ capstone-frontend/app/components/Navbar.jsx
@@ -55,12 +55,33 @@ export default function Example() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 pb-4 pt-2">
-              <Disclosure.Button
-                as="a"
-                href="#"
-                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
+            <Disclosure.Button
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 w-full text-left"
               >
-                About
+                <Link href='/'>
+                    Home
+                </Link>
+              </Disclosure.Button>
+            <Disclosure.Button
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 w-full text-left"
+              >
+                <Link href='/topwords'>
+                    Top Words
+                </Link>
+              </Disclosure.Button>
+            <Disclosure.Button
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 w-full text-left"
+              >
+                <Link href='/data'>
+                    Data
+                </Link>
+              </Disclosure.Button>
+              <Disclosure.Button
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 w-full text-left"
+              >
+                <Link href='/about'>
+                    About
+                </Link>
               </Disclosure.Button>
             </div>
           </Disclosure.Panel>
